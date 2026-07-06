9120601df7fe0d0f7ceabfa44ac8d8b7772ff5ca
kciwebinfo-dot <kciwebinfo@gmail.com>
2026-07-06 13:33:55 +0530

    Add JS cache busting

--- includes/scripts.php
+++ includes/scripts.php
@@ -13,8 +13,8 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
 <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
-<script src="<?= e(asset('assets/js/ajax.js')) ?>"></script>
-<script src="<?= e(asset('assets/js/theme.js')) ?>"></script>
-<script src="<?= e(asset('assets/js/app.js')) ?>"></script>
+<script src="<?= e(asset('assets/js/ajax.js')) ?>?v=20260706-2"></script>
+<script src="<?= e(asset('assets/js/theme.js')) ?>?v=20260706-2"></script>
+<script src="<?= e(asset('assets/js/app.js')) ?>?v=20260706-2"></script>
 </body>
 </html>
