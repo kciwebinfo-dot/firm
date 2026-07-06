435dddb0b86de99bbd056fc7eaa1900720586cab
kciwebinfo-dot <kciwebinfo@gmail.com>
2026-07-06 13:03:28 +0530

    Add layout footer

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+    </section>
+    <footer class="app-footer">
+        <?= e(firm('footer_text', 'Copyright ' . date('Y') . ' ' . firm('firm_name', 'Tax Firm'))) ?>
+    </footer>
+</main>
+</div>
